2d871db58010ea0c464b859211391638fbebda0e
Petr Stepchenko <nitrinodev@gmail.com>
2019-05-04 02:03:56 +0300

    Add resolver data

--- lib/dynamic_schema_web/resolvers/custom_objects_resolver.ex
+++ lib/dynamic_schema_web/resolvers/custom_objects_resolver.ex
@@ -6,6 +6,10 @@ defmodule DynamicSchemaWeb.CustomObjectsResolver do
 
   def all_objects(_root, _args, _info) do
     # objects = CustomObjects.list_objects()
-    {:ok, [%{data: %{count: 2, title: "new title"}}]}
+    {:ok,
+     [
+       %{data: %{first_name: "Petr", last_name: "Stepchenko", age: 27, phone: "+79208457152"}},
+       %{data: %{first_name: "Albert", last_name: "Inshtein", age: 27, phone: "+79208400000"}}
+     ]}
   end
 end
